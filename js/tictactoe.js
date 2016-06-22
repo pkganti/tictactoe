04d31cd3e6f655b5bbf03de5ab32b84e82b7289c
Phani Ganti <phaniganti@Phanis-MacBook-Pro.local>
2016-06-22 14:23:41 +1000

    Modified the project files

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,86 +1,64 @@
-//This function takes the player moves and returns an array
-var player1Move = function(value){
-  var moves = [];
-  moves.push(value);
-  return moves;
-};
-
-//This function takes the player moves and returns an array
-var player2Move = function(value){
-  var moves = [];
-  moves.push(value);
-  return moves;
-};
-
 //This is the object which has array of elements of winning combinations
+
+var playerArr = ['-','-','-','-','-','-','-','-','-'];
+var count=0;
 var threeObject = {
-  totalMoves: {one: "0",two: '1',
-               three: '2', four: '3',
-               five: '4', six: '5',
-               seven: '6',eight: '7',
-               nine: '8'}
+    winPos: [[0,1,2], [0, 3, 6], [0, 4, 8], [3, 4, 5], [6, 7, 8], [1, 4, 7], [2, 5, 8], [2, 4, 6]]
   };
-var winPos = [[0,1,2], [0, 3, 6], [0, 4, 8], [3, 4, 5], [6, 7, 8], [2, 4, 7], [2, 5, 8], [2, 4, 6]];
-//This function iterates through the object array and compares the player move array and
-//returns the winner
-// var winCombination = function(){
-//   for(var i=0; i<threeObject.wins.length; i++){
-//
-//   if(player1Move().length===3 || player2Move.length===3)
-//   {
-//     if((threeObject.wins[i].split(",").join()===player1Move.split(".").join()) || (threeObject.wins[i].split(",").join()===player1Move.arrayReverse().split(".").join()))
-//     {
-//       return 'Player1';
-//     }
-//     else if((threeObject.wins[i].split(",").join()===player2Move.split(".").join()) || (threeObject.wins[i].split(",").join()===player2Move.arrayReverse().split(".").join())){
-//       return 'Player2';
-//     }
-//     else {
-//       return 'Draw';
-//     }
-//   }
-//
-// }
-// };
 
-//This function reverses the array input
-var arrayReverse = function(array){
-  return array.reverse();
-};
-
-//
-
-var getPlayer = function(){
-var count;
-var player1='X';
-var player2='O';
-var player;
-
-for(count=0; count<10; count++){
-  if(count%2===0){
-    player = player1;
+//This function takes an argument of player type and displays which player has won
+var getWinner = function(player){
+  // console.log(player);
+if(player!==null){
+  var winner = null;
+  main: for(var i=0; i<threeObject.winPos.length; i++){
+    var matchingMoves = 0;
+    for(var j=0; j<threeObject.winPos[i].length; j++){
+      if(playerArr[threeObject.winPos[i][j]]===player){
+        matchingMoves += 1;
+      }
+      
+    }
+    if (matchingMoves === 3) {
+        // console.log("Player " + player + " wins");
+        winner = player;
+        break main;
+    }
   }
-  else{
-    player = player2;
-  }
-return player;
+  return winner;
 }
+else {
+  return null;
+}
+
 };
 
+//This function iterates for 9 times and return a player name each time
+var getPlayer = function(){
 
-var position = function(){
-$('.box').on('click', function(){
-    var position = $(this).attr('Id');
-    console.log(position);
-    return position;
+  var player1='X';
+  var player2='O';
+  var currentplayer;
 
-    // playSquare(x, y);
-});
+  if(count%2===0 && count<9){
+      currentplayer = player1;
+    }
+    else if(count%2!==0 && count<9){
+      currentplayer = player2;
+    }
+    else{
+      currentplayer = null;
+    }
+    count=count+1;
+
+  return currentplayer;
 };
 
-var playerMark = function(player){
-  var position = position();
-  var playerArr = [];
-  playerArr[threeObject.totalMoves.position]=playerName;
-  console.log(playerArr);
+//This function marks the position returned by the above function with the player name
+
+var playerMark = function(position, player){
+  if(player!==null){
+  playerArr[position]=player;
+  // return playerArr;
+  }
 };
--- js/tictactoe2.js
+++ js/tictactoe2.js
@@ -1,16 +1,62 @@
+$(document).ready(function(){
 
-var position = function(){
-$('.box').on('click', function(){
-    var position = $(this).attr('Id');
-    return position;
+var player1Count = 0;
+var player2Count = 0;
 
-    // playSquare(x, y);
+$('player1').on('click', function(){
+  src+=$(this).attr('src');
+  return src;
 });
-};
 
-var playerMark = function(playerName){
-  var position = position();
-  var playerArr = [];
-  playerArr[threeObject.totalMoves.position]=playerName;
-  console.log(playerArr);
-};
+$('.box').on('click', function(){
+  if ($(this).hasClass('played')) {
+    return; // already played here
+  }
+  $(this).addClass('played');
+
+//The below function checks for the class with gameover and displays the message
+  var player = getPlayer();
+  var position = $(this).attr('Id');
+// console.log(position);
+// console.log(playerArr);
+  playerMark(position, player);
+  $(this).text(player);
+
+  if(getWinner(player)!==null) {
+    if(player==='X'){
+      player1Count+=1;
+    }
+    else{
+      player2Count+=1;
+    }
+    $('#boxgrid').addClass('gameover');
+    alert('Game won by : '+player);
+    $('.player1').attr('placeholder',player1Count);
+    $('.player2').attr('placeholder',player2Count);
+    $('.box').empty();
+    $('#boxgrid').removeClass('gameover');
+    $('.box').removeClass('played');
+    playerArr = ['-','-','-','-','-','-','-','-','-'];
+    count=0;
+    }
+  // if ( $(this).closest('#boxgrid.gameover').length > 0 ) {
+    // alert('Game won by : '+player);
+    // $('.box').empty();
+    // $('#boxgrid').removeClass('gameover');
+    // $('.box').removeClass('played');
+    // playerArr = ['-','-','-','-','-','-','-','-','-'];
+    // count=0;
+// }
+// }
+  if(count===9){
+    $('#boxgrid').addClass('gamedrawn');
+    alert("The game is drawn");
+    $('.box').empty();
+    $('#boxgrid').removeClass('gamedrawn');
+    $('.box').removeClass('played');
+    playerArr = ['-','-','-','-','-','-','-','-','-'];
+    count=0;
+  }
+// });
+});
+});
